27050911575114f3a2e090a09ac52353e6ba0b18
kovsi <kovsiun@gmail.com>
2022-02-23 15:18:02 +0100

    JAVAGAMELESSON1

--- core/src/com/star/app/StarGame.java
+++ core/src/com/star/app/StarGame.java
@@ -1,37 +1,49 @@
 package com.star.app;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	float x;
-	
+	private SpriteBatch batch;
+	private Background background;
+	private Hero hero;
+	private Asteroid asteroid;
+
+	public Hero getHero() {
+		return hero;
+	}
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("redbackground2.jpg");
+		background = new Background(this);
+		hero = new Hero();
+//		asteroid = new Asteroid();
 	}
 
 	@Override
 	public void render () {
-		update();
+		float dt = Gdx.graphics.getDeltaTime();
+		update(dt);
 		ScreenUtils.clear(0.5f, 0.5f , 0.5f, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		background.render(batch);
+		hero.render(batch);
+//		asteroid.render(batch);
 		batch.end();
 	}
 
-	public void update () {
-		x += 1;
+	public void update (float dt) {
+	background.update(dt);
+	hero.update(dt);
+//	asteroid.update(dt);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
